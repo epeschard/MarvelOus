930c0fa85232c61c832b9db34991e5d9cd2afb8a
Eugene Peschard <e@peschard.me>
2017-05-07 18:01:18 +0200

    Added tests for Thumbnail download, JSON download & Updated Comics.json & testLoadLocalJSONtoRealm()

--- MarvelOusTests/MarvelOusTests.swift
+++ MarvelOusTests/MarvelOusTests.swift
@@ -11,10 +11,14 @@ import RealmSwift
 
 @testable import MarvelOus
 
+var sessionUnderTest: URLSession!
+let realm = try! Realm()
+
 class TestCaseRealm: XCTestCase {
   
   override func setUp() {
     super.setUp()
+    sessionUnderTest = URLSession(configuration: URLSessionConfiguration.default)
     // Use an in-memory Realm identified by the name of the current test.
     // This ensures that each test can't accidentally access or modify the data
     Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
@@ -35,10 +39,139 @@ class MarvelOusTests: TestCaseRealm {
     }
     
     // when
-    let count = try! Realm().objects(Comic.self).count
+    let characterCount = realm.objects(Character.self).count
+    let characterListCount = realm.objects(CharacterList.self).count
+    let comicCount = realm.objects(Comic.self).count
+    let comicSummaryCount = realm.objects(ComicSummary.self).count
+    let creatorCount = realm.objects(Creator.self).count
+    let creatorListCount = realm.objects(CreatorList.self).count
+    let datesCount = realm.objects(Dates.self).count
+    let imagesCount = realm.objects(Images.self).count
+    let priceCount = realm.objects(Price.self).count
+    let seriesCount = realm.objects(Series.self).count
+    let textObjectCount = realm.objects(TextObject.self).count
+    let thumbnailCount = realm.objects(Thumbnail.self).count
+    let urlCount = realm.objects(Url.self).count
     
     // then
-    XCTAssertEqual(count, 10)
+    XCTAssertEqual(characterCount, 8)
+    XCTAssertEqual(characterListCount, 10)
+    XCTAssertEqual(comicCount, 10)
+    XCTAssertEqual(comicSummaryCount, 0)
+    XCTAssertEqual(creatorCount, 22)
+    XCTAssertEqual(creatorListCount, 10)
+    XCTAssertEqual(datesCount, 31)
+    XCTAssertEqual(imagesCount, 11)
+    XCTAssertEqual(priceCount, 11)
+    XCTAssertEqual(seriesCount, 6)
+    XCTAssertEqual(textObjectCount, 10)
+    XCTAssertEqual(thumbnailCount, 10)
+    XCTAssertEqual(urlCount, 22)
+  }
+  
+  // Asynchronous test: faster fail
+  func testPhotoFetchCompletes() {
+    // given
+    let path = "http://i.annihil.us/u/prod/marvel/i/mg/3/40/5900b1d977e02"
+//    let url = URL(string: "\(path)/standard_xlarge.jpg")!
+    // store XCTestExpectation in promise
+    let promise =  expectation(description: "Completion handler invoked")
+    var statusCode: Int?
+    var responseError: Error?
+    
+    // when
+    if let url = URL(string: "\(path)/standard_xlarge.jpg") {
+      let dataTask = sessionUnderTest.dataTask(with: url) {
+        data, response, error in
+        statusCode = (response as? HTTPURLResponse)?.statusCode
+        responseError = error
+        // to match the description
+        promise.fulfill()
+      }
+      dataTask.resume()
+    }
+    // keep the test running until all expectations are fulfilled, or reach timeout
+    waitForExpectations(timeout: 5, handler: nil)
+    
+    // then
+    XCTAssertNil(responseError)
+    XCTAssertEqual(statusCode, 200)
+  }
+  
+  func testGetThumbnailData() {
+    // given
+    let thumbnail = Thumbnail()
+    let path = "http://i.annihil.us/u/prod/marvel/i/mg/3/40/5900b1d977e02"
+    // store XCTestExpectation in promise
+    let promise =  expectation(description: "Completion handler invoked")
+    var statusCode: Int?
+    var responseError: Error?
+    // when
+    MarvelAPI().getData(for: thumbnail, from: path) {
+      dataResponse in
+      statusCode = dataResponse.response?.statusCode
+      responseError = dataResponse.error
+      // to match the description
+      if dataResponse.data != nil {
+        promise.fulfill()
+      }
+    }
+    // keep the test running until all expectations are fulfilled, or reach timeout
+    waitForExpectations(timeout: 5, handler: nil)
+    // then
+    XCTAssertNil(responseError)
+    XCTAssertEqual(statusCode, 200)
+  }
+  
+  func testJsonDownload() {
+    // given
+    // store XCTestExpectation in promise
+    let promise =  expectation(description: "Completion handler invoked")
+    var statusCode: Int?
+    var responseError: Error?
+    // when
+    MarvelAPI().download(.comics, first: 100, startingFrom: 0) {
+      dataResponse in
+      statusCode = dataResponse.response?.statusCode
+      responseError = dataResponse.error
+      // to match the description
+      if dataResponse.result.value is [String: Any] {
+        promise.fulfill()
+      }
+    }
+    // keep the test running until all expectations are fulfilled, or reach timeout
+    waitForExpectations(timeout: 5, handler: nil)
+    // then
+    XCTAssertNil(responseError)
+    XCTAssertEqual(statusCode, 200)
+  }
+  
+  func testRemoteJsonFetchCompletes() {
+    // given
+    let path = "http://i.annihil.us/u/prod/marvel/i/mg/3/40/5900b1d977e02"
+    //    let url = URL(string: "\(path)/standard_xlarge.jpg")!
+    // store XCTestExpectation in promise
+    let promise =  expectation(description: "Completion handler invoked")
+    var statusCode: Int?
+    var responseError: Error?
+    
+    // when
+    if let url = URL(string: "\(path)/standard_xlarge.jpg") {
+      let dataTask = sessionUnderTest.dataTask(with: url) {
+        data, response, error in
+        statusCode = (response as? HTTPURLResponse)?.statusCode
+        responseError = error
+        // to match the description
+        promise.fulfill()
+      }
+      dataTask.resume()
+    }
+    // keep the test running until all expectations are fulfilled, or reach timeout
+    waitForExpectations(timeout: 5, handler: nil)
+    
+    // then
+    XCTAssertNil(responseError)
+    XCTAssertEqual(statusCode, 200)
   }
   
   override func setUp() {
